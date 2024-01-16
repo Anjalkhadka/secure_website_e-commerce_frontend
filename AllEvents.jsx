fe5f4c50b300bfdf08fc93446d9265deae6036d2
Anjalkhadka <85629558+Anjalkhadka@users.noreply.github.com>
2024-01-16 16:43:00 +0545

    Update AllEvents.jsx

--- AllEvents.jsx
+++ AllEvents.jsx
@@ -1,4 +1,10 @@
-
+import { Button } from "@material-ui/core";
+import { DataGrid } from "@material-ui/data-grid";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {  AiOutlineEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { server } from "../../server";
 
 const AllEvents = () => {
   const [events, setEvents] = useState([]);
