f867b4fbad7740afd3b3da22300f1f057ea36217
Anjalkhadka <85629558+Anjalkhadka@users.noreply.github.com>
2024-01-16 16:40:02 +0545

    Update AdminDashboardMain.jsx

--- AdminDashboardMain.jsx
+++ AdminDashboardMain.jsx
@@ -10,8 +10,7 @@ import { getAllOrdersOfAdmin } from "../../redux/actions/order";
 import Loader from "../Layout/Loader";
 import { getAllSellers } from "../../redux/actions/sellers";
 
-const AdminDashboardMain = () => {
-  const dispatch = useDispatch();
+
 
   const { adminOrders,adminOrderLoading } = useSelector((state) => state.order);
   const { sellers } = useSelector((state) => state.seller);
